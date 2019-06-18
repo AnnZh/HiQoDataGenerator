8e851ed9d076aead26c5d26a1f3100a8f475d955
DenisKaminsky <deniskaminsky123@mail.ru>
2019-06-18 17:49:30 +0300

    Create Field Type Model and FieldTypesContext for EF.

---
+++ DataGenerator/WebApp/Models/DataContexts/FieldTypesContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models.DataContexts
+{
+    public class FieldTypesContext: DbContext
+    {
+        public DbSet<FieldType> FieldTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=DataGeneratorDatabase;Username=postgres;Password=denis123");
+        }
+    }
+}
