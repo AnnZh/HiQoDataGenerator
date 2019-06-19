647570aa14e12e5152a4b8ff5d2fd2b67012cdf2
Artem S1lentem <tema.yarmosh@gmail.com>
2019-06-19 14:00:08 +0300

    finished design project struct

---
+++ DataGeneratorServer/src/ApplicationCore/Models/Timezone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Models
+{
+    public class Timezone
+    {
+        public int Id { get; }
+        public string Value { get; }
+    }
+}
---
+++ DataGeneratorServer/src/Infrastructure/Repositories/EFRepositories/TimezoneRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Interfaces.Repositories;
+
+namespace Infrastructure.Repositories.EFRepositories
+{
+    class TimezoneRepository: ITimezonesRepository
+    {
+    }
+}
