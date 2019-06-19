cf57253da71c092c65b38b900d0bf6fdd148e21a
DenisKaminsky <36419821+DenisKaminsky@users.noreply.github.com>
2019-06-19 12:17:20 +0300

    Update Startup.cs

--- DataGeneratorServer/src/WebApp/Startup.cs
+++ DataGeneratorServer/src/WebApp/Startup.cs
@@ -22,8 +22,7 @@ namespace WebApp
         {
             Configuration = configuration;
         }
-    
-    
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
