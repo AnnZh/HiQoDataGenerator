fd7dc4c8e6e21031e41a326bff5d90733b16eae9
DenisKaminsky <deniskaminsky123@mail.ru>
2019-06-19 11:14:32 +0300

    Add new models

---
+++ DataGeneratorServer/src/Infrastructure/Models/ConstraintModels/ConstraintValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Infrastructure.Models.CustomObjectModels;
+
+namespace Infrastructure.Models.ConstraintModels
+{
+    public class ConstraintValue
+    {
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string Value { get; set; }
+       
+        public int ConstraintId { get; set; }//FK
+        public Constraint Constraint { get; set; }//Navigation property
+        
+    }
+}
